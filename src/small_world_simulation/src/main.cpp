e2379515ad16b6650554c7d1fad5794117adbc8d
Joshua Williams <joshmackwilliams@protonmail.com>
2021-10-13 09:11:13 -0500

    Add boost program options library

--- src/small_world_simulation/src/main.cpp
+++ src/small_world_simulation/src/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main() {
+#include "small_world_io/CsvEnrollmentDataReader.hpp"
+#include "small_world_simulation/EnrollmentData.hpp"
+#include "small_world_simulation/Simulation.hpp"
+
+int main(int argc, char ** argv) {
+  boost::program_options::options_description options("Options");
+  options.add_options()
+    ("help", "Display help and exit")
+    ("parameters", "The location of the parameter file")
+  ;
+
+  boost::program_options::variables_map options_map;
+  boost::program_options::store(boost::program_options::parse_command_line(
+    argc, argv, options), options_map);
+  boost::program_options::notify(options_map);
+
+  if(options_map.count("help")) {
+    std::cout << options << std::endl;
+    return 1;
+  }
+
   std::cout << "Hello World! (simulation)" << std::endl;
 }
